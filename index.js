1afe629c6540124bee81a5a3093cc8a0813345e8
BurcuMengu <burcumengu@gmail.com>
2024-07-14 15:09:44 +0300

    push

--- index.js
+++ index.js
@@ -36,6 +36,42 @@ let totalCorrect = 0;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let currentQuestion = {};
+
+//Get home page
+app.get("/", async (req, res) => {
+    totalCorrect = 0;
+    await nextQuestion();
+    console.log(currentQuestion);
+    res.render("index.ejs", { question: currentQuestion });
+});
+
+//Post a new post
+app.post("/submit", async (req, res) => {
+    let answer = req.body.answer.trim();
+    let isCorrect = false;
+    if(currentQuestion.name.toLowerCase() === answer.toLowerCase()) {
+        totalCorrect++;
+        console.log(totalCorrect);
+        isCorrect = true;
+    }
+
+    nextQuestion();
+    res.render("index.ejs", {
+        question: currentQuestion,
+        wasCorrect: isCorrect,
+        totalScore: totalCorrect,
+    });
+});
+
+async function nextQuestion() {
+    const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
+
+    currentQuestion = randomCountry;
+}
+
+
+
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
